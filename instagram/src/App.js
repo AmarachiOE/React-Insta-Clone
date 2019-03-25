c1df616e47339981c85bd4330f85ebbe76793d5d
AmarachiOE <ao.ejiawoko@gmail.com>
2019-03-25 12:48:29 -0700

    Updated dummy-data.js for working image urls

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'; // instead of class App extends React.
 import './App.css';
 import dummyData from "./dummy-data"; // don't us { dummyData } because not JSON object?
 import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from "./components/PostContainer/PostContainer";
+import CommentSection from "./components/CommentSection/CommentSection";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: dummyData,
+      dummyData: dummyData,
     };
   }
   render() {
@@ -15,6 +17,8 @@ class App extends Component {
       <div className="App">
         <h2>Hi</h2>
         <SearchBar />
+        <PostContainer />
+        <CommentSection />
       </div>
     );
   }
