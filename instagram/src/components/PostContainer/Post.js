f722a14d6eb3dc78860a33087cfca92406c13e0d
AmarachiOE <ao.ejiawoko@gmail.com>
2019-03-25 17:16:10 -0700

    moved CommentSection invocation from PostContainer to Post and added some styles to username info

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import CommentSection from "../CommentSection/CommentSection";
 
 const Post = props => {
     return (
-        <div>Post</div>
+        <div>
+            <div className="user-info">
+                <img className="thumbnail" src={props.eachPost.thumbnailUrl} alt="Thumbnail" />
+                <p><strong>{props.eachPost.username}</strong></p>
+            </div>
+            <img src={props.eachPost.imageUrl} alt="The of Post"/>
+            <CommentSection eachPost={props.eachPost} key={props.eachPost.id} />
+        </div>
     );
 }
 
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./PostContainer.css";
-import CommentSection from "/Users/Amarachi/Documents/LAMBDA/VS Code/React-Insta-Clone/instagram/src/components/CommentSection/CommentSection";
+// import CommentSection from "/Users/Amarachi/Documents/LAMBDA/VS Code/React-Insta-Clone/instagram/src/components/CommentSection/CommentSection";
 import Post from "./Post";
 
 // Each PostContainer component will then pass the array of comments on each post object as a prop to an instance of the CommentSection component
@@ -13,9 +13,9 @@ const PostContainer = props => {
     return (
         <div>
             <p>This is the Post Container</p>
-            <Post />
-            {props.commentProp.map( post => <CommentSection eachPost={post} key={post.id} />
-           
+            {/* {props.commentProp.map( post => <CommentSection eachPost={post} key={post.id} />)} */}
+            {props.commentProp.map( post => 
+            <Post eachPost={post} key={post.id} />)}           
             )}
 
         </div>
