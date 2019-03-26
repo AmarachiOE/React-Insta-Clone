a56390f3556d00e59ce6753491788cd8a15eb3b3
AmarachiOE <ao.ejiawoko@gmail.com>
2019-03-25 18:33:44 -0700

    added icons and more styling to post container

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -3,7 +3,7 @@ import CommentSection from "../CommentSection/CommentSection";
 
 // We're looking at all 3 posts here
 
-// for each post there will be user info (thumbnail and username), image post, and comment section
+// for each post there will be user info (thumbnail and username), image post, icons, like counter, and comment section
 
 // no mapping here, just establishing content areas for each post
 
@@ -14,7 +14,16 @@ const Post = props => {
                 <img className="thumbnail" src={props.eachPost.thumbnailUrl} alt="Thumbnail" />
                 <p><strong>{props.eachPost.username}</strong></p>
             </div>
+
             <img className="post-image"src={props.eachPost.imageUrl} alt="The Post"/>
+
+            <div className="post-icons">
+                <i className="far fa-heart"> </i>
+                <i className="far fa-comment"></i>
+            </div>
+
+            <p className="post-likes">123 likes</p>
+
             <CommentSection eachPost={props.eachPost}  />
         </div>
     );
