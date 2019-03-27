3b002361c2a1b502968b3e9cad0e6290cb29a15a
AmarachiOE <ao.ejiawoko@gmail.com>
2019-03-27 12:18:28 -0700

    Successfully transferred code from App.js to PostsPage.js and webpage still functions as before. Rendered <PostsPage /> on App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,59 +1,16 @@
 import React, { Component } from "react"; // instead of class App extends React.Component
 import "./App.css";
-import dummyData from "./dummy-data"; // don't us { dummyData } because not JSON object?
-import SearchBar from "./components/SearchBar/SearchBar";
-import PostContainer from "./components/PostContainer/PostContainer";
-//import CommentSection from "./components/CommentSection/CommentSection";
 import PostsPage from "./components/PostContainer/PostsPage";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      dummyData: [],
-      filteredPost: [], // for search bar
-      searching: false
-    };
-  }
+  // constructor() {
+  //   super();
+  // }
 
-  componentDidMount() {
-    this.setState({ dummyData: dummyData });
-  }
-
-  // Filter Post function for Search Bar
-
-  
-
-  
-  filterPostHandler = event => {
-    
-    const newDummyData = this.state.dummyData.filter( (currentPost) => {
-      if (currentPost.username.includes(event.target.value)) {
-        return currentPost;
-      } 
-    });
-
-    if (event.target.value === "") {
-      this.setState({ searching: false });
-    } else {
-      this.setState ({ filteredPost: newDummyData, searching: true });
-    }
-
-    
-  }
-  
-
-  // App should map over dummyData, passing each individual object as a prop to an instance of PostContainer
 
   render() {
     return (
       <div className="App">
-        <SearchBar 
-        filterPostHandler={this.filterPostHandler}
-        />
-        {/* TERNARY statement: if searching = true then use filteredPost data, if false use dummyData. PostContainer is what is renering the data to the screen as posts*/}
-        <PostContainer 
-        dummyData={this.state.searching ? this.state.filteredPost : this.state.dummyData} />
         <PostsPage />
       </div>
     );
