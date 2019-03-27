f8942cdfaa7be485668b048d9ea8339f26d58d98
AmarachiOE <ao.ejiawoko@gmail.com>
2019-03-27 12:43:10 -0700

    Built beginning stages of withAuthenticate HOC and rendered it on App.js. Webpage still functions as before.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react"; // instead of class App extends React.Component
 import "./App.css";
 import PostsPage from "./components/PostContainer/PostsPage";
+import withAuthenticate from "./components/Authentication/withAuthenticate";
+
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage);
 
 class App extends Component {
   // constructor() {
   //   super();
   // }
 
-
   render() {
     return (
       <div className="App">
-        <PostsPage />
+        <ComponentFromWithAuthenticate />
       </div>
     );
   }
--- instagram/src/components/Authentication/withAuthenticate.js
+++ instagram/src/components/Authentication/withAuthenticate.js
@@ -1,10 +1,37 @@
 import React from 'react';
 
-const withAuthenticate = props => {
-    if () {
-      return
-    } //else
-    return
-  }
+/*
+
+Building the High Order Component
+
+Create a directory called authentication
+Inside that directory create a HOC called withAuthenticate. This is where all of the magic is going to happen.
+
+This component should be able to take in a component as an argument, and it will return a class component. 
+(so shouldn't have: = props =>)
+
+Inside of withAuthenticate's render method, you'll want to return the Component that gets passed into it.
+
+Be sure to export.
+
+Head over to App.js and import in our new withAuthenticate Higher Order Component.
+
+Set a new const called ComponentFromWithAuthenticate, and set it's value to the HOC invoked, with PostsPage passed in.
+
+Inside App, you should now render ComponentFromWithAuthenticate in place of PostsPage.
+
+If this worked correctly, then everything should render as it used to.
+
+withAuthenticate will look a lot like this when you're done setting it up.
+
+*/
+
+const withAuthenticate = PassedComponent => 
+    class extends React.Component {
+        render() {
+            return <PassedComponent />
+        }
+    };
+  
 
 export default withAuthenticate;
