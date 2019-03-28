ea64e7b47d470c202c85e6f9bf97927e6525c2f2
AmarachiOE <ao.ejiawoko@gmail.com>
2019-03-28 13:10:38 -0700

    replaced css with styled-components on Posts.js

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -1,6 +1,48 @@
 import React from "react";
 import CommentSection from "../CommentSection/CommentSection";
 import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
+
+const EachPostContainerDiv = styled.div`
+  margin-bottom: 50px;
+  border: 1px solid lightgray;
+`;
+
+const UserInfoDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const ThumbnailImg = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  padding: 10px;
+`;
+
+const UserInfoP = styled.p`
+  padding-top: 15px;
+  font-weight: bold;
+`;
+
+const PostImg = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+const PostIconsDiv = styled.div`
+  padding: 12px 0;
+  display: flex;
+  color: black; 
+  font-size: 1.5rem;
+
+`;
+
+const PostLikesP = styled.p`
+  padding-left: 12px;
+`;
+
 
 // We're looking at all 3 posts here
 
@@ -30,35 +72,33 @@ class Post extends React.Component {
   // Must change props.whatever to this.props.whatever
   render() {
     return (
-      <div className="each-post">
-        <div className="user-info">
-          <img
-            className="thumbnail"
+      <EachPostContainerDiv>
+        <UserInfoDiv>
+          <ThumbnailImg
             src={this.props.eachPost.thumbnailUrl}
             alt="Thumbnail"
           />
-          <p>
-            <strong>{this.props.eachPost.username}</strong>
-          </p>
-        </div>
+          <UserInfoP>
+            {this.props.eachPost.username}
+          </UserInfoP>
+        </UserInfoDiv>
 
-        <img
-          className="post-image"
+        <PostImg
           src={this.props.eachPost.imageUrl}
           alt="The Post"
         />
 
-        <div className="post-icons">
+        <PostIconsDiv>
           <i className="far fa-heart" onClick={this.increaseLikes}>
             {" "}
           </i>
           <i className="far fa-comment" />
-        </div>
+        </PostIconsDiv>
 
-        <p className="post-likes">{this.state.likesCounter} likes</p>
+        <PostLikesP>{this.state.likesCounter} likes</PostLikesP>
 
         <CommentSection eachPost={this.props.eachPost} />
-      </div>
+      </EachPostContainerDiv>
     );
   }
 }
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -2,6 +2,13 @@ import React from 'react';
 import "./PostContainer.css";
 // import CommentSection from "/Users/Amarachi/Documents/LAMBDA/VS Code/React-Insta-Clone/instagram/src/components/CommentSection/CommentSection";
 import Post from "./Post";
+import styled, { css } from "styled-components";
+
+const PostContainerDiv = styled.div`
+    width: 604px;
+    margin-left: 10%;
+    margin-top: 20px;
+`;
 
 
 // Each PostContainer component will then pass the array of comments on each post object as a prop to an instance of the CommentSection component
@@ -17,10 +24,10 @@ import Post from "./Post";
 const PostContainer = props => {
     return (
         //console.log("Post Container:", props),
-        <div className="post-container">
+        <PostContainerDiv>
             {props.dummyData.map( post => 
             <Post eachPost={post} key={post.id} />)}           
-        </div>
+        </PostContainerDiv>
     );
 };
 
